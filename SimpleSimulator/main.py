b7685d9626b61666c1ea88f7eef867b04c49277d
pragyanyadav <pragyan20226@iiitd.ac.in>
2021-08-24 18:53:37 +0530

    main and graph

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -173,3 +173,121 @@ def memDump(instructions):
             count += 1
     for i in range(count, 256):
         print('0'*16)
+
+def graph(progList, cycle):
+    progList = np.array(progList)
+    cycle = np.array(cycle)
+    # print(progList)
+    # print(cycle)
+    plt.scatter(cycle, progList, color='red')
+    plt.plot(cycle, progList)
+    plt.title('Simulator Scatter Plot')
+    plt.xlabel('Cycle Number')
+    plt.ylabel('Memory Address')
+    plt.savefig('graph.png')
+
+def main():
+    instructions = {} #dict of instructions
+    global variables, progCount, progList, i
+    variables = {} #dict of variables
+    t = 0 #temp
+    progCount = 0 #program counter
+    progList = [] #list of program counter
+
+    while True:
+        try:
+            line = input().strip()
+            if line != '':
+                instructions[t] = line
+                t += 1
+        except EOFError:
+            break
+
+    # varIndex = len(instructions)
+    for i in range(len(instructions)):
+        if instructions[i][0:5] in [OPCODE['st'], OPCODE['ld']]:
+            variables[bin2dec(instructions[i][8:16])] = 0
+    
+    i = 0 #instruction count
+    hltornot = None
+    hltrun = False
+    while instructions[i][0:5] not in [OPCODE['hlt']]:
+        if instructions[i][0:5] in [OPCODE['add'], OPCODE['sub'], OPCODE['mul'], OPCODE['xor'], OPCODE['or'], OPCODE['and']]:
+            #typeA
+            oldFlag = regValues['FLAGS']
+            typeA(instructions[i][0:5], instructions[i][7:10], instructions[i][10:13], instructions[i][13:16])
+            if oldFlag == regValues['FLAGS']:
+                regValues['FLAGS'] = 0
+            printIns()
+            progList.append(progCount)
+            progCount += 1
+            i += 1
+        elif instructions[i][0:5] in [OPCODE['mov'], OPCODE['rs'], OPCODE['ls']]:
+            #typeB
+            oldFlag = regValues['FLAGS']
+            typeB(instructions[i][0:5], instructions[i][5:8], instructions[i][8:16])
+            if oldFlag == regValues['FLAGS']:
+                regValues['FLAGS'] = 0
+            printIns()
+            progList.append(progCount)
+            progCount += 1
+            i += 1
+        elif instructions[i][0:5] in [OPCODE['mov2'], OPCODE['div'], OPCODE['not'], OPCODE['cmp']]:
+            #typeC
+            oldFlag = regValues['FLAGS']
+            typeC(instructions[i][0:5], instructions[i][10:13], instructions[i][13:16])
+            if oldFlag == regValues['FLAGS']:
+                regValues['FLAGS'] = 0
+            printIns()
+            progList.append(progCount)
+            progCount += 1
+            i += 1
+        elif instructions[i][0:5] in [OPCODE['ld'], OPCODE['st']]:
+            #typeD
+            oldFlag = regValues['FLAGS']
+            typeD(instructions[i][0:5], instructions[i][5:8], instructions[i][8:16])
+            if oldFlag == regValues['FLAGS']:
+                regValues['FLAGS'] = 0
+            printIns()
+            progList.append(progCount)
+            progCount += 1
+            i += 1
+        elif instructions[i][0:5] in [OPCODE['jmp'], OPCODE['jlt'], OPCODE['jgt'], OPCODE['je']]:
+            #typeE
+            # oldFlag = regValues['FLAGS']
+            oldi = i
+            oldFlag = regValues['FLAGS']
+            # print(oldi, progCount)
+            print(f'{dec2bin(progCount, 8)} {dec2bin(regValues["R0"])} {dec2bin(regValues["R1"])} {dec2bin(regValues["R2"])} {dec2bin(regValues["R3"])} {dec2bin(regValues["R4"])} {dec2bin(regValues["R5"])} {dec2bin(regValues["R6"])} {dec2bin(0)}')
+            progList.append(progCount)
+            typeE(instructions[i][0:5], instructions[i][8:16])
+            if oldFlag == regValues['FLAGS']:
+                regValues['FLAGS'] = 0
+            if oldi == i:
+                progCount += 1
+                i += 1
+            # print(i, progCount)
+            # printIns()
+            hltornot = instructions[i][0:5]
+        if hltornot in [OPCODE['hlt']]:
+            #typeF
+            # typeF(instructions[i][0:5])
+            hltrun = True
+            # print('in if')
+            printIns()
+            progList.append(progCount)
+            break
+    if hltrun == False and instructions[i][0:5] in [OPCODE['hlt']]:
+        #typeF
+        # typeF(instructions[i][0:5])
+        printIns()
+        progList.append(progCount)
+        # print('outif')
+    memDump(instructions)
+    cycle = [x for x in range(len(progList))]
+    # print(progList)
+    # print(cycle)
+    # graph(progList, cycle)
+
+if __name__ == '__main__':
+    main()
