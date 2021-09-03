012a82c272e54d0cc71f48ebc647c349295c874d
pragyanyadav <pragyan20226@iiitd.ac.in>
2021-09-03 19:17:35 +0530

    assm

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -53,46 +53,66 @@ def hltcheck(statements):
     #check for hlt instruction
     h_count = 0
     for i in range(len(statements)):
-        if statements[i][0] == 'hlt' or 'hlt' in statements[i]:
-            h_count += 1
-        if h_count > 1:
-            print(f'More than 1 hlt is given on input line {i+1}')
-            quit()
+        if len(statements[i]) != 0:
+            if statements[i][0] == 'hlt' or 'hlt' in statements[i]:
+                h_count += 1
+    for i in range(len(statements)):
+        if len(statements[i]) != 0:
+            if h_count > 1:
+                print(f'More than 1 hlt is given on input line {i+1}')
+                quit()
+            elif h_count < 1:
+                print(f'No hlt in the given instructions.')
+                quit()
 
 def varcount(statements, var):
     #counts the number of times a variable is used
     v_count = 0
     for i in range(len(statements)):
-        if len(statements[i]) > 1 and statements[i][1] == var:
-            v_count += 1
+        if len(statements[i]) != 0:
+            if len(statements[i]) > 1 and statements[i][1] == var:
+                v_count += 1
     return v_count
 
 def varin(statements):
     #checks for variables in between instructions
     tvar = {}
     for i in range(len(statements)):
-        if statements[i][0] != 'var':
-            break
-        tvar[i] = statements[i]
+        if len(statements[i]) != 0:
+            if statements[i][0] != 'var':
+                break
+            tvar[i] = statements[i]
 
     for i in range(len(tvar), len(statements)):
-        if statements[i][0] == 'var':
-            print(f'Variables declared in between instructions in input line {i+1}')
-            quit()
+        if len(statements[i]) != 0:
+            if statements[i][0] == 'var':
+                print(f'Variables declared in between instructions in input line {i+1}')
+                quit()
 
 def lblcheck(statements):
     #check for labels
-    label = {}
-    lbl = 0
+    label = []
+    # lbl = 0
     for i in range(len(statements)):
-        if ':' in statements[i][0]:
-            label[lbl] = statements[i]
-            lbl += 1
-        if statements[i][0] in ['jmp', 'jlt', 'jgt', 'je']:
-            for j in range(len(label)):
-                if statements[i][2] not in label[j][0].rstrip(':'):
+        if len(statements[i]) != 0:
+            if ':' in statements[i][0]:
+                label.append(statements[i][0].rstrip(':'))
+    # print(label)
+    for i in range(len(statements)):
+        if len(statements[i]) != 0:
+                # label[lbl] = statements[i]
+                # lbl += 1
+            if statements[i][0] in ['jmp', 'jlt', 'jgt', 'je']:
+                if len(label) == 0:
                     print(f'Undefined label called on line {i+1}')
                     quit()
+                else:
+                    for j in range(len(label)):
+                        # print(statements[i][0], statements[i][1], label[j])
+                        # print(label)
+                        if statements[i][1] not in label:
+                            print(f'Undefined label called on line {i+1}')
+                            quit()
 
 def in_errorhandler(statements):
     #check for errors in input file
@@ -103,11 +123,12 @@ def in_errorhandler(statements):
     hltcheck(statements)
 
     for i in range(len(statements)):
-        if statements[i][0] == 'var':
-            vcount = varcount(statements, statements[i][1])
-            if vcount > 1:
-                print(f'2 or more variables with same name are passed on input line {i+1}')
-                quit()
+        if len(statements[i]) != 0:
+            if statements[i][0] == 'var':
+                vcount = varcount(statements, statements[i][1])
+                if vcount > 1:
+                    print(f'2 or more variables with same name are passed on input line {i+1}')
+                    quit()
     
     varin(statements)
     lblcheck(statements)
@@ -116,45 +137,47 @@ def in_errorhandler(statements):
 def inschecker(instructions):
     #check for errors in instruction
     for i in range(len(instructions)):
-        if ':' in instructions[i][0]:
-            if len(instructions[i][1:len(instructions[i])]) == 0:
-                print(f'Wrong ISA instruction on line {i + 1}')
-                quit()
-            if instructions[i][1] in ['add', 'sub', 'mul', 'xor', 'or', 'and'] and len(instructions[i]) != 5:
+        if len(instructions[i]) != 0:
+            if ':' in instructions[i][0]:
+                if len(instructions[i][1:len(instructions[i])]) == 0:
+                    print(f'Wrong ISA instruction on line {i + 1}')
+                    quit()
+                if instructions[i][1] in ['add', 'sub', 'mul', 'xor', 'or', 'and'] and len(instructions[i]) != 5:
+                    print(f'Wrong ISA instruction on line {i+1}')
+                    quit()
+                elif instructions[i][1] in ['mov', 'ld', 'st', 'div', 'rs', 'ls', 'not', 'cmp'] and len(instructions[i]) != 4:
+                    print(f'Wrong ISA instruction on line {i+1}')
+                    quit()
+                elif instructions[i][1] in ['jmp', 'jlt', 'jgt', 'je', 'var'] and len(instructions[i]) != 3:
+                    print(f'Wrong ISA instruction on line {i+1}')
+                    quit()
+                elif instructions[i][1] == 'hlt' and len(instructions[i]) != 2:
+                    print(f'Wrong ISA instruction on line {i+1}')
+                    quit()
+            if instructions[i][0] == 'var' and len(instructions[i][1:len(instructions[i])]) == 0:
                 print(f'Wrong ISA instruction on line {i+1}')
                 quit()
-            elif instructions[i][1] in ['mov', 'ld', 'st', 'div', 'rs', 'ls', 'not', 'cmp'] and len(instructions[i]) != 4:
+            elif instructions[i][0] in ['add', 'sub', 'mul', 'xor', 'or', 'and'] and len(instructions[i]) != 4:
                 print(f'Wrong ISA instruction on line {i+1}')
                 quit()
-            elif instructions[i][1] in ['jmp', 'jlt', 'jgt', 'je', 'var'] and len(instructions[i]) != 3:
+            elif instructions[i][0] in ['mov', 'ld', 'st', 'div', 'rs', 'ls', 'not', 'cmp'] and len(instructions[i]) != 3:
                 print(f'Wrong ISA instruction on line {i+1}')
                 quit()
-            elif instructions[i][1] == 'hlt' and len(instructions[i]) != 2:
+            elif instructions[i][0] in ['jmp', 'jlt', 'jgt', 'je', 'var'] and len(instructions[i]) != 2:
                 print(f'Wrong ISA instruction on line {i+1}')
                 quit()
-        if instructions[i][0] == 'var' and len(instructions[i][1:len(instructions[i])]) == 0:
-            print(f'Wrong ISA instruction on line {i+1}')
-            quit()
-        elif instructions[i][0] in ['add', 'sub', 'mul', 'xor', 'or', 'and'] and len(instructions[i]) != 4:
-            print(f'Wrong ISA instruction on line {i+1}')
-            quit()
-        elif instructions[i][0] in ['mov', 'ld', 'st', 'div', 'rs', 'ls', 'not', 'cmp'] and len(instructions[i]) != 3:
-            print(f'Wrong ISA instruction on line {i+1}')
-            quit()
-        elif instructions[i][0] in ['jmp', 'jlt', 'jgt', 'je', 'var'] and len(instructions[i]) != 2:
-            print(f'Wrong ISA instruction on line {i+1}')
-            quit()
-        elif instructions[i][0] == 'hlt' and len(instructions[i]) != 1:
-            print(f'Wrong ISA instruction on line {i+1}')
-            quit()
+            elif instructions[i][0] == 'hlt' and len(instructions[i]) != 1:
+                print(f'Wrong ISA instruction on line {i+1}')
+                quit()
     return None
 
 def flagerror(instructions):
     #check for errors in flags
     for i in range(len(instructions)):
-        if instructions[i][0] != 'mov' and 'FLAGS' in instructions[i][1: len(instructions[i])]:
-            print(f'Flag Error on line {i+1}')
-            quit()
+        if len(instructions[i]) != 0:
+            if instructions[i][0] != 'mov' and 'FLAGS' in instructions[i][1: len(instructions[i])]:
+                print(f'Flag Error on line {i+1}')
+                quit()
 
 def varrepeat(variables):
     #check for repeat variables
@@ -164,34 +187,47 @@ def varrepeat(variables):
                 print(f'Variable Name should be unique. Repeated on line {wovariables+j+1}')
                 quit()
 
-def varcheck(instructions, variables):
+def varcheck(instructions):
     #check for variables if defined or not
+    variables = []
     for i in range(len(instructions)):
-        if instructions[i][0] in ['ld', 'st']:
-            for j in range(len(variables)):
-                if variables[j][1] not in instructions[i][1:len(instructions[i])]:
-                    print(f'Undefined variable called on line {i+1}')
-                    quit()
+        if len(instructions[i]) != 0:
+            if instructions[i][0] == 'var':
+                variables.append(instructions[i][1])
+    for i in range(len(instructions)):
+        if len(instructions[i]) != 0:
+            if instructions[i][0] in ['ld', 'st']:
+                for j in range(len(variables)):
+                    # print(instructions[i][1:len(instructions[i])])
+                    # print(variables[j])
+                    if instructions[i][2] not in variables:
+                        print(f'Undefined variable called on line {i+1}')
+                        quit()
 
 def errorhandler(instructions):
     #main error handler
     for i in range(len(instructions)):
-        if instructions[i][0] not in OPCODE and instructions[i][0] != 'var' and ':' not in instructions[i][0]:
-            print(f'Wrong ISA Syntax on line {i+1}')
-            quit()
-        if instructions[i][0] == 'mov' and '$' in instructions[i][2]:
-            if int(instructions[i][2].strip('$')) not in range(0, 256):
-                print(f'Illegal Immediate Value (less than 0 or more than 255) on line {i+1}')
+        if len(instructions[i]) != 0:
+            if instructions[i][0] not in OPCODE and instructions[i][0] != 'var' and ':' not in instructions[i][0]:
+                print(f'Wrong ISA Syntax on line {i+1}')
                 quit()
-        elif ':' in instructions[i][0]:
-            if ':' in instructions[i][1:len(instructions[i])]:
-                print(f'Nesting of labels is illegal at line {i+1}')
-                quit()
-            else:
-                for j in range(len(instructions)):
-                    if instructions[i][0] == instructions[j][0] and i != j:
-                        print(f'Label Name should be unique. Repeated on line {j+1}')
-                        quit()
+            if instructions[i][0] == 'mov' and '$' in instructions[i][2]:
+                if '.' in instructions[i][2]:
+                    print(f'Input number on line {i+1} is not an integer.')
+                    quit()
+                if int(instructions[i][2].strip('$')) not in range(0, 256):
+                    print(f'Illegal Immediate Value (less than 0 or more than 255) on line {i+1}')
+                    quit()
+            elif ':' in instructions[i][0]:
+                if ':' in instructions[i][1:len(instructions[i])]:
+                    print(f'Nesting of labels is illegal at line {i+1}')
+                    quit()
+                else:
+                    for j in range(len(instructions)):
+                        if len(instructions[j]) != 0:
+                            if instructions[i][0] == instructions[j][0] and i != j:
+                                print(f'Label Name should be unique. Repeated on line {j+1}')
+                                quit()
 
 def typeA(op, r1, r2, r3):
     #type A: 3 register type
@@ -244,18 +280,20 @@ def main():
                 t += 1
         except EOFError:
             break
+    # print(statements)
     inscheck = inschecker(statements)
     inerror = in_errorhandler(statements)
 
     ins = 0
     var = 0
     for i in range(len(statements)):
-        if statements[i][0] == 'var':
-        	variables[var] = statements[i]
-        	var+=1                       	         
-        else:
-            instructions[ins] = statements[i]
-            ins += 1
+        if len(statements[i]) != 0:
+            if statements[i][0] == 'var':
+                variables[var] = statements[i]
+                var+=1                       	         
+            else:
+                instructions[ins] = statements[i]
+                ins += 1
     
     # print(instructions)
     wovariables = len(instructions)
@@ -269,7 +307,7 @@ def main():
     # print(variables)
     # print(wovariables)
     varrepeat(variables)
-    varcheck(statements, variables)
+    varcheck(statements)
     flagerror(statements)
     errorhandler(statements)
     for i in range(len(instructions)):
@@ -310,22 +348,22 @@ def main():
                 typeC(OPCODE['cmp'], instructions[i][2], instructions[i][3])
             elif instructions[i][1] == 'jmp':
                 for j in range(len(instructions)):
-                    if instructions[i][2] == instructions[j][0]:
+                    if instructions[i][2] == instructions[j][0].rstrip(':'):
                         mem = j
                         typeE(OPCODE['jmp'], mem)
             elif instructions[i][1] == 'jlt':
                 for j in range(len(instructions)):
-                    if instructions[i][2] == instructions[j][0]:
+                    if instructions[i][2] == instructions[j][0].rstrip(':'):
                         mem = j
                         typeE(OPCODE['jlt'], mem)
             elif instructions[i][1] == 'jgt':
                 for j in range(len(instructions)):
-                    if instructions[i][2] == instructions[j][0]:
+                    if instructions[i][2] == instructions[j][0].rstrip(':'):
                         mem = j
                         typeE(OPCODE['jgt'], mem)
             elif instructions[i][1] == 'je':
                 for j in range(len(instructions)):
-                    if instructions[i][2] == instructions[j][0]:
+                    if instructions[i][2] == instructions[j][0].rstrip(':'):
                         mem = j
                         typeE(OPCODE['je'], mem)
             elif instructions[i][1] == 'hlt':
